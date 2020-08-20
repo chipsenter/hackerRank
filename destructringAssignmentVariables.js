e25ff371377cc692f7f36032db594e0068120c01
freddan1980@gmail.comm <freddan1980@gmail.com>
2020-08-20 09:19:53 -0600

    destructuring

---
+++ destructringAssignmentVariables.js
@@ -0,0 +1,16 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  
+const {today: highToday} = HIGH_TEMPERATURES;
+const {tomorrow: highTomorrow} = HIGH_TEMPERATURES; 
+const {yesterday: highYesterday} = HIGH_TEMPERATURES;
+// Only change code above this line
+
+console.log(highToday);
+console.log(highTomorrow);
+console.log(highYesterday);
